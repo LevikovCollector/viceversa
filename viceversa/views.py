e779708555bb4ee51e5477aee7440254c1d6ebe6
Vladimir Levikov <v.v.levikov@gmail.com>
2022-05-31 21:32:21 +0300

    Ficha reverse with word count

--- viceversa/views.py
+++ viceversa/views.py
@@ -6,5 +6,6 @@ def home(request):
 
 def reverse(request):
     user_text = request.GET['usertext']
+    word_count = len(user_text.split(" "))
     reverse_text = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext': user_text, 'reversetext': reverse_text})
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversetext': reverse_text, 'wordcount':word_count})
